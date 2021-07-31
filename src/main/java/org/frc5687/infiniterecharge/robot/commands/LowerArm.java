ca44fe150ad4d77b9f662b2dd1f16323bc606622
deslobodzian <de.slobodzian@gmail.com>
2021-07-31 16:19:00 -0400

    Working shooter stuff

---
+++ src/main/java/org/frc5687/infiniterecharge/robot/commands/LowerArm.java
@@ -0,0 +1,9 @@
+package org.frc5687.infiniterecharge.robot.commands;
+
+import org.frc5687.infiniterecharge.robot.subsystems.Climber;
+
+public class LowerArm extends OutliersCommand {
+    private Climber
+    public LowerArm(Climber climer) {
+    }
+}
